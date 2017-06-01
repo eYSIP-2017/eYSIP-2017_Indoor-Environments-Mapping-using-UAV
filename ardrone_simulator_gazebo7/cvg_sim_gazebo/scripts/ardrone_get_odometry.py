3f5c30dfb0457c6e607405291e21797ea4c57c55
Rishabh Beri <rishabhberi2606@gmail.com>
2017-06-01 18:08:55 +0530

    Added a python script to publish odometry data of the ardrone in gazebo

---
+++ ardrone_simulator_gazebo7/cvg_sim_gazebo/scripts/ardrone_get_odometry.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import rospy
+from nav_msgs.msg import *
+from gazebo_msgs.msg import *
+import tf
+
+odom_data = Odometry()
+
+odom_data.header.frame_id = 'odom'
+odom_data.child_frame_id = 'quadrotor'
+
+rospy.init_node('OdometryData')
+pub_odom = rospy.Publisher('/ardrone/odometry', Odometry, queue_size=1)
+pub_tf = tf.TransformBroadcaster()
+
+def callback(data):
+	# print data
+	odom_data.header.stamp = rospy.Time.now()
+	quadrotor_index = data.name.index("quadrotor")
+	odom_data.pose.pose = data.pose[quadrotor_index]
+	odom_data.twist.twist = data.twist[quadrotor_index]
+	pub_odom.publish(odom_data)
+	pub_tf.sendTransform((odom_data.pose.pose.position.x, odom_data.pose.pose.position.y, odom_data.pose.pose.position.z), (odom_data.pose.pose.orientation.x, odom_data.pose.pose.orientation.y, odom_data.pose.pose.orientation.z, odom_data.pose.pose.orientation.w), odom_data.header.stamp, 'quadrotor', 'odom')
+
+rospy.Subscriber('/gazebo/model_states', ModelStates, callback)
+
+rospy.spin()
