58389159df17f0451200e74a56af5d2845639372
Rishabh Beri <rishabhberi2606@gmail.com>
2017-07-05 20:54:57 +0530

    Added a script to clone the repository and setup the environment

---
+++ setup.sh
@@ -0,0 +1,11 @@
+cd ~/catkin_ws/src
+git clone https://github.com/eYSIP-2017/eYSIP-2017_Indoor-Environments-Mapping-using-UAV.git
+mv eYSIP-2017_Indoor-Environments-Mapping-using-UAV IndoorEnvironmentMappingUsingUA
+cd ~/catkin_ws/src/IndoorEnvironmentMappingUsingUA/bash_scripts
+chmod a+x *.sh
+cd ~/catkin_ws/src/IndoorEnvironmentMappingUsingUA/ardrone_simulator_gazebo7/cvg_sim_gazebo/scripts
+chmod a+x *.py
+cd ~/catkin_ws/src/IndoorEnvironmentMappingUsingUA/firebird6/scripts
+chmod a+x *.py
+cd ~/catkin_ws/src/IndoorEnvironmentMappingUsingUA/realsense_gazebo_plugin/scripts
+chmod a+x *.py
