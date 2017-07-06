f6363fe307e1cbfb7fbf59991bbde46b31d0901c
Samaahita S Belavadi <samaahita.1996@gmail.com>
2017-07-06 13:04:56 +0530

    fixed error

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 cd ~/catkin_ws/src
 git clone https://github.com/eYSIP-2017/eYSIP-2017_Indoor-Environments-Mapping-using-UAV.git
 mv eYSIP-2017_Indoor-Environments-Mapping-using-UAV IndoorEnvironmentMappingUsingUAV
-cd ~/catkin_ws/src/IndoorEnvironmentMappingUsingUA/bash_scripts
+cd ~/catkin_ws/src/IndoorEnvironmentMappingUsingUAV/bash_scripts
 chmod a+x *.sh
-cd ~/catkin_ws/src/IndoorEnvironmentMappingUsingUA/ardrone_simulator_gazebo7/cvg_sim_gazebo/scripts
+cd ~/catkin_ws/src/IndoorEnvironmentMappingUsingUAV/ardrone_simulator_gazebo7/cvg_sim_gazebo/scripts
 chmod a+x *.py
-cd ~/catkin_ws/src/IndoorEnvironmentMappingUsingUA/firebird6/scripts
+cd ~/catkin_ws/src/IndoorEnvironmentMappingUsingUAV/firebird6/scripts
 chmod a+x *.py
-cd ~/catkin_ws/src/IndoorEnvironmentMappingUsingUA/realsense_gazebo_plugin/scripts
+cd ~/catkin_ws/src/IndoorEnvironmentMappingUsingUAV/realsense_gazebo_plugin/scripts
 chmod a+x *.py
 echo "export GAZEBO_RESOURCE_PATH=$GAZEBO_RESOURCE_PATH:/home/$USER" >> ~/.bashrc
